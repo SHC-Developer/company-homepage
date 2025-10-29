d3290248a7836012a6d70c019023b9833ce12718
SHC-Developer <bluesangh@gmail.com>
2025-10-29 17:46:34 +0900

    Update YouTube video ID to ZFqlZC77oQw

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -17,7 +17,7 @@ export const HeroSection = () => {
           <div className="relative w-full h-full">
             <iframe
               className="absolute top-1/2 left-1/2 w-[177.77vh] h-[56.25vw] min-h-full min-w-full transform -translate-x-1/2 -translate-y-1/2"
-              src="https://www.youtube-nocookie.com/embed/GMc61qGjRHo?autoplay=1&mute=1&loop=1&playlist=GMc61qGjRHo&controls=0&rel=0&modestbranding=1&playsinline=1&start=0&iv_load_policy=3&fs=0&cc_load_policy=0&disablekb=1&si=Ts-rGv1xDCn2hB4G"
+              src="https://www.youtube-nocookie.com/embed/ZFqlZC77oQw?autoplay=1&mute=1&loop=1&playlist=ZFqlZC77oQw&controls=0&rel=0&modestbranding=1&playsinline=1&start=0&iv_load_policy=3&fs=0&cc_load_policy=0&disablekb=1&si=Ts-rGv1xDCn2hB4G"
               title="대한민국 상이군경회 시설사업소 배경 영상"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
